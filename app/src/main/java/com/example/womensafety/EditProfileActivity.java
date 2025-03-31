35a06c20f24513694a7946fa7918155ca7556517
bLadeAyush <prasadom9411@gmail.com>
2025-03-31 20:03:06 +0530

    Error fix

--- app/src/main/java/com/example/womensafety/EditProfileActivity.java
+++ app/src/main/java/com/example/womensafety/EditProfileActivity.java
@@ -1,18 +1,16 @@
 package com.example.womensafety;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
